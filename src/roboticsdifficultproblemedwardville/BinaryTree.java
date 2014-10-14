515eebf03242b4443e61e819205981dd2d606524
s-xuch <s-xuch@54L1808-09.it.bsd405.org>
2014-10-14 08:42:39 -0700

    Added distance value to binary tree

--- src/roboticsdifficultproblemedwardville/BinaryTree.java
+++ src/roboticsdifficultproblemedwardville/BinaryTree.java
@@ -12,8 +12,8 @@ package roboticsdifficultproblemedwardville;
 public class BinaryTree {
 
     private int tree[][][];
-    private int x = 0;
-    private int y = 0;
+    public int length = 0;
+    public int y = 0;
 
     public BinaryTree(int numberofnodes, int numberofedges) { //number of edges refers to the number of connections a node can have
         tree = new int[numberofnodes][numberofedges][2];
@@ -24,10 +24,12 @@ public class BinaryTree {
     }
 
     public void setNode(int value) {
-        tree[x][0][0] = value;
-        x++;
+        tree[length][0][0] = value;
+        length++;
     }
-
+    public void setDistance(int position,int value){
+        tree[position][0][1]=value;
+    }
     public void setEdge(int nodea, int nodeb) {
         int a;
         for (a = 0; tree[nodea][a][0] != 0; a++) {
