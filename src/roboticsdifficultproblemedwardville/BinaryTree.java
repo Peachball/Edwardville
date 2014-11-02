45945d821bef9f7348938d29bac3d0a8fdf37632
Chen <Chen@PeachballsPC>
2014-11-01 19:46:19 -0700

    finished the solution, but has not debugged

--- src/roboticsdifficultproblemedwardville/BinaryTree.java
+++ src/roboticsdifficultproblemedwardville/BinaryTree.java
@@ -13,9 +13,10 @@ public class BinaryTree {
 
     private int tree[][][];
     private int values[];
+    private int numEdges[];
     private int distances[][];
     private int distancecounter[];
-    public int length = 0;
+    public int length;
     public int y = 0;
 
     //number of edges refers to the number of connections a node can have
@@ -42,15 +43,39 @@ public class BinaryTree {
     public void setValue(int position, int value) {
         tree[position][0][0] = value;
     }
+    
+    public int numberOfNodes(){
+        return tree.length;
+    }
 
     public int getValue(int position) {
         return tree[position][0][0];
     }
 
-    public int getDistance(int position) {
-        return tree[position][0][1];
+    //Edge num should be from 0 to the number of nodes - 1
+
+    public int findEdgeConnection(int position, int edgenum) {
+        return tree[position][edgenum + 1][0];
     }
 
+    public int findEdgeValue(int position, int edgenum) {
+        if (tree[position][edgenum][1] < 0) {
+            return -tree[position][edgenum + 1][1];
+        } else {
+            return tree[position][edgenum + 1][1];
+        }
+    }
+
+    public boolean edgeDirection(int position, int edgenum) {
+        if (tree[position][edgenum + 1][1] < 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+//    public int getDistance(int position) {
+//        return tree[position][0][1];
+//    }
 //    public int findPosition(int value) {
 //        for (int x = 0; x < tree.length; x++) {
 //            if (tree[x][0][0] == value) {
@@ -59,40 +84,41 @@ public class BinaryTree {
 //        }
 //        return -1;
 //    }
-    public int findPosition(int distance) {
-        if (distances[distance][0] != 0) {
-            return distances[distance][0];
-        }
-        for (int x = 0; x < tree.length; x++) {
-            if (tree[x][0][1] == distance) {
-                return x;
-            }
-        }
-        return -1;
-    }
-
-    public void setNode(int value) {
-        tree[length][0][0] = value;
-        values[length] = value;
-        length++;
-    }
-
-    public void setDistance(int position, int value) {
-        tree[position][0][1] = value;
-        distances[value][distancecounter[position]] = position;
-        distancecounter[position]++;
-    }
+//    public int findPosition(int distance) {
+//        if (distances[distance][0] != 0) {
+//            return distances[distance][0];
+//        }
+//        for (int x = 0; x < tree.length; x++) {
+//            if (tree[x][0][1] == distance) {
+//                return x;
+//            }
+//        }
+//        return -1;
+//    }
+// Note: The "position" of each node will be equivalent to the "distance"
+// it is from the intial stop
+//    public void setNode(int value) {
+//        tree[length][0][0] = value;
+//        values[length] = value;
+//        length++;
+//    }
 
     public void setEdge(int nodea, int nodeb, int cost) {
         int a;
         for (a = 0; tree[nodea][a][0] != 0; a++) {
         }
-        tree[nodea][a][0] = nodeb;
-        tree[nodea][a][1] = cost;
+        tree[nodea][a][0] = nodeb; //0 is which other node it is connected to
+        tree[nodea][a][1] = cost;  //1 is the cost of the train to get to the other node
+        numEdges[nodea] = a;
         for (a = 0; tree[nodeb][a][0] != 0; a++) {
         }
         tree[nodeb][a][0] = nodea;
         tree[nodeb][a][1] = -cost;
+        numEdges[nodeb] = a;
+    }
+
+    public int findnumEdges(int node) {
+        return numEdges[node];
     }
     //positive is true
     //negative is false
--- src/roboticsdifficultproblemedwardville/RoboticsDifficultProblemEdwardville.java
+++ src/roboticsdifficultproblemedwardville/RoboticsDifficultProblemEdwardville.java
@@ -1,56 +1,85 @@
 package roboticsdifficultproblemedwardville;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+
 import java.util.Scanner;
 
 public class RoboticsDifficultProblemEdwardville {
 
     public static void main(String[] args) throws FileNotFoundException {
+        RoboticsDifficultProblemEdwardville recursion = new RoboticsDifficultProblemEdwardville();
         int trains[][] = input();
-        binarytree(trains);
+        BinaryTree trainfile = binarytree(trains);
+        System.out.println(recursion.treeSolver(trainfile, 0));
     }
 
-    public static void binarytree(int trainfile[][]) {
-        int[][][] buffer = converter(trainfile);
-        BinaryTree file = new BinaryTree(buffer[0][0].length, buffer[0].length);
-        file.setNode(0);
-        file.setDistance(0,0);
-        for(int a =0;;){
-        for (int counter = 0; counter < trainfile.length; counter++) {
-            
+    public int treeSolver(BinaryTree file, int position) {
+        int costs[] = new int[file.findnumEdges(position)];
+        int cheapest;
+        if(file.findnumEdges(position)==0){
+            return file.findEdgeValue(position, 0);
         }
+        for (int x = 0; x < file.findnumEdges(position); x++) {
+            if(file.edgeDirection(position, x)&&file.findEdgeConnection(position, x)!=file.numberOfNodes()){
+                costs[x]=treeSolver(file,file.findEdgeConnection(position, x));
+            } else{
+                costs[x]=file.findEdgeValue(position,x);
+            }
         }
+        Arrays.sort(costs);
+        cheapest = costs[0];
+        return cheapest;
     }
 
-    public static int[][][] converter(int trainfile[][]) {
-        int file[][][] = new int[2][trainfile.length / 2][trainfile.length];
-        for (int a = 0; a < file.length; a++) {
-            int buffer = 0;
-            for (int b = 0; trainfile[a][b] != 0; b++) {
-                file[0][a][trainfile[a][b]] = trainfile[a * 2 + 1][b];
-                if (b!=0) {
-                    file[1][a][trainfile[a][buffer]] = b;
-                }
-                buffer = b;
+    public static BinaryTree binarytree(int trainfile[][]) {
+//        int[][][] buffer = converter(trainfile);
+// Hopefully the above line will not be implemented, but I'm not sure whether
+// it has to be implemented eventually or not
+        BinaryTree file = new BinaryTree(trainfile[0].length, trainfile.length / 2);
+        file.setValue(0, 0);
+        for (int a = 0; a < trainfile.length / 2; a++) {
+            for (int counter = 0; counter < trainfile[0].length
+                    && trainfile[a * 2][counter + 1] != 0; counter++) {
+                file.setEdge(trainfile[a * 2][counter], trainfile[a * 2][counter + 1], trainfile[a * 2 + 1][counter]);
             }
         }
         return file;
-        // the 0 part of the array is the distance+train section
-        //the 1 part is the part to help search
     }
 
+//    public static int[][][] converter(int trainfile[][]) {
+//        int file[][][] = new int[2][trainfile.length / 2][trainfile.length];
+//        for (int a = 0; a < file.length; a++) {
+//            int buffer = 0;
+//            for (int b = 0; trainfile[a][b] != 0; b++) {
+//                file[0][a][trainfile[a][b]] = trainfile[a * 2 + 1][b];
+//                if (b != 0) {
+//                    file[1][a][trainfile[a][buffer]] = b;
+//                }
+//                buffer = b;
+//            }
+//        }
+//        return file;
+//        // the 0 part of the array is the distance+train section
+//        //the 1 part is the part to help search
+//    }
     public static int[][] input() throws FileNotFoundException {
-        Scanner reader = new Scanner(System.in);
+        Scanner reader = new Scanner(new FileInputStream("trainfile.txt"));
         int x;
         int y;
         int z;
         x = reader.nextInt(); //x is number of train lines
         y = reader.nextInt(); //y is the total distance
         int file[][] = new int[2 * x][y];
-        for (x = 0; x < file.length; x++) {
+        for (x = 0; x < file.length && reader.hasNextInt(); x += 2) {
             z = reader.nextInt();
-            for (y = 0; y < z; y++) {
+            for (y = 0; y < z && reader.hasNextInt(); y++) {
                 file[x][y] = reader.nextInt();
+            }
+            for (y = 0; y < z - 1 && reader.hasNextInt(); y++) {
+                file[x + 1][y] = reader.nextInt();
             }
         }
         return file;
